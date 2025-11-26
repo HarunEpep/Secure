8a721de7a74c24b715272f2f03bb19c2717dfd68
HarunEpep <127110813+HarunEpep@users.noreply.github.com>
2025-11-26 17:44:56 +0700

    Delete raw.js

--- raw.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  // Daftar token whitelist:
-  const whitelist = [
-    "Token"
-  ];
-
-  // Kirim response JSON
-  res.status(200).json(whitelist);
-}
